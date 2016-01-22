1cb200465afef2558c724380ffe6b8943f1ecd10
Tyler Flint <tylerflint@gmail.com>
2016-01-22 09:30:59 -0700

    Generating a mock server for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'shaman'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:any, /127.0.0.1/).to_rack(FakeShaman)
+  end
+end
